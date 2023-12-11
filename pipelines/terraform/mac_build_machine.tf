ad9c1e6270d38ad40a151082aed6f64209f8160c
Carlos Lapao <cjlapao@gmail.com>
2023-12-11 09:13:26 +0000

    enable build

--- pipelines/terraform/mac_build_machine.tf
+++ pipelines/terraform/mac_build_machine.tf
@@ -1,56 +1,56 @@
-# locals {
-#   builder_name = "mac-${var.runner_name}"
-#   host         = data.terraform_remote_state.host.outputs.parallels-desktop_host[0]
-#   api_key      = data.terraform_remote_state.host.outputs.parallels-desktop_api_key[0]
-# }
+locals {
+  builder_name = "mac-${var.runner_name}"
+  host         = data.terraform_remote_state.host.outputs.parallels-desktop_host[0]
+  api_key      = data.terraform_remote_state.host.outputs.parallels-desktop_api_key[0]
+}
 
 
-# data "parallels-desktop_vm" "mac_builder_base" {
-#   host = local.host
+data "parallels-desktop_vm" "mac_builder_base" {
+  host = local.host
 
-#   filter {
-#     field_name       = "name"
-#     value            = "macos14-github-actions-runner-base"
-#     case_insensitive = true
-#   }
-# }
+  filter {
+    field_name       = "name"
+    value            = "macos14-github-actions-runner-base"
+    case_insensitive = true
+  }
+}
 
-# resource "parallels-desktop_clone_vm" "mac_builder" {
-#   count = length(data.parallels-desktop_vm.mac_builder_base.machines)
+resource "parallels-desktop_clone_vm" "mac_builder" {
+  count = length(data.parallels-desktop_vm.mac_builder_base.machines)
 
-#   authenticator {
-#     api_key = local.api_key
-#   }
+  authenticator {
+    api_key = local.api_key
+  }
 
-#   host       = local.host
-#   name       = local.builder_name
-#   owner      = "ec2-user"
-#   base_vm_id = data.parallels-desktop_vm.mac_builder_base.machines[count.index].id
-#   path       = "/Users/ec2-user/Parallels"
+  host       = local.host
+  name       = local.builder_name
+  owner      = "ec2-user"
+  base_vm_id = data.parallels-desktop_vm.mac_builder_base.machines[count.index].id
+  path       = "/Users/ec2-user/Parallels"
 
-#   config {
-#     start_headless = true
-#   }
+  config {
+    start_headless = true
+  }
 
-#   force_changes = true
+  force_changes = true
 
-#   post_processor_script {
-#     retry {
-#       attempts              = 4
-#       wait_between_attempts = "10s"
-#     }
-#     inline = [
-#       "curl -o /tmp/install-runner.sh https://raw.githubusercontent.com/Parallels/prlctl-scripts/main/github/actions-runner/mac/install-runner.sh",
-#       "chmod +x /tmp/install-runner.sh",
-#       "sudo /tmp/install-runner.sh -o ${var.github_org_name} -t ${var.github_token} -p /opt -u parallels -n ${local.builder_name} -l macos,macos14,xcode",
-#     ]
-#   }
+  post_processor_script {
+    retry {
+      attempts              = 4
+      wait_between_attempts = "10s"
+    }
+    inline = [
+      "curl -o /tmp/install-runner.sh https://raw.githubusercontent.com/Parallels/prlctl-scripts/main/github/actions-runner/mac/install-runner.sh",
+      "chmod +x /tmp/install-runner.sh",
+      "sudo /tmp/install-runner.sh -o ${var.github_org_name} -t ${var.github_token} -p /opt -u parallels -n ${local.builder_name} -l macos,macos14,xcode",
+    ]
+  }
 
-#   on_destroy_script {
-#     inline = [
-#       "curl  -o /tmp/remove-runner.sh https://raw.githubusercontent.com/Parallels/prlctl-scripts/main/github/actions-runner/mac/remove-runner.sh",
-#       "chmod +x remove-runner.sh",
-#       "sudo /tmp/remove-runner.sh -o ${var.github_org_name} -t ${var.github_token}  -p /opt -u parallels",
-#     ]
-#   }
-# }
+  on_destroy_script {
+    inline = [
+      "curl  -o /tmp/remove-runner.sh https://raw.githubusercontent.com/Parallels/prlctl-scripts/main/github/actions-runner/mac/remove-runner.sh",
+      "chmod +x remove-runner.sh",
+      "sudo /tmp/remove-runner.sh -o ${var.github_org_name} -t ${var.github_token}  -p /opt -u parallels",
+    ]
+  }
+}
